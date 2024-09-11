36b07a6c5128516f5171518f8ec534b6937563e6
diffenyu <ydfyudingfeng@gmail.com>
2024-09-11 21:27:29 +0800

    refine dockerfile

---
+++ nvidia_sample/docker/build/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -ex
+
+IMAGE_NAME=nvidia/cuda:12.2.2-devel-ubuntu22.04_nvcodec_0.1
+
+# Proxy settings
+PROXY_SETTINGS=""
+if [ "${http_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg http_proxy=${http_proxy}"
+fi
+
+if [ "${https_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg https_proxy=${https_proxy}"
+fi
+
+if [ "${ftp_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg ftp_proxy=${ftp_proxy}"
+fi
+
+if [ "${no_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg no_proxy=${no_proxy}"
+fi
+
+if [ "$PROXY_SETTINGS" != "" ]; then
+  echo "Proxy settings were found and will be used during build."
+fi
+
+echo "$http_proxy"
+
+# ################## #
+# BUILDING THE IMAGE #
+# ################## #
+echo "Building image '$IMAGE_NAME' ..."
+
+# BUILD THE IMAGE (replace all environment variables)
+BUILD_START=$(date '+%s')
+#docker build --force-rm=$NOCACHE --no-cache=$NOCACHE $PROXY_SETTINGS -t $IMAGE_NAME -f Dockerfile . || {
+docker build $PROXY_SETTINGS -t $IMAGE_NAME -f Dockerfile . || {
+  echo "There was an error building the image."
+  exit 1
+}
+BUILD_END=$(date '+%s')
+BUILD_ELAPSED=`expr $BUILD_END - $BUILD_START`
+
